ff7b27bb4c763f963329d51059b897d24ddfb009
Nilson Augusto Moraes <Nilson Augusto Moraes>
2017-02-01 23:58:59 -0300

    correção das variáveis trapezoidal simples

--- rules/trapezoidalSimples.m
+++ rules/trapezoidalSimples.m
@@ -1,3 +1,3 @@
-function trapezoidalSimples(a, b, f) %a e b é o intervalo, f é a função.
-    return (b-a)*(f(a)+f(b))/2; %fórmula trapezoidal.
+function trapezoidalSimples(limInf, limSup, f) %a e b é o intervalo, f é a função.
+    return (limSup-limInf)*(f(limInf)+f(limSup))/2; %fórmula trapezoidal.
 end
